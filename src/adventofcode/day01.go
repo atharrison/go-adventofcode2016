739f7415e635f1dc64a1c7517db0ec87f2931cc7
Andrew Harrison <aharrison1@underarmour.com>
2016-11-28 15:31:37 -0600

    Initial framework

---
+++ src/adventofcode/day01.go
@@ -0,0 +1,8 @@
+package adventofcode
+
+import "fmt"
+
+func Day01() {
+	fmt.Println("Day01 START")
+
+}
---
+++ src/main/aoc.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"adventofcode"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("AdventOfCode2016 Runner")
+
+	adventofcode.Day01()
+
+	fmt.Println("\nComplete")
+}
