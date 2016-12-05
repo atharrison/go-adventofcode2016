f2ccb6b0184ef754021f9f569a0fd0bedc087a3c
Andrew Harrison <aharrison1@underarmour.com>
2016-12-04 23:34:19 -0600

    Day 5 Part 2

--- src/adventofcode/day05.go
+++ src/adventofcode/day05.go
@@ -3,7 +3,7 @@ package adventofcode
 import (
 	"crypto/md5"
 	"fmt"
-	//"os"
+	"strconv"
 )
 
 func Day05() {
@@ -11,19 +11,49 @@ func Day05() {
 	input := "uqwqemis"
 	//input := "abc"
 
+	////Part 1
+	//index := 0
+	//var code string
+	//for len(code) < 8 {
+	//	toHash := fmt.Sprintf("%v%v", input, index)
+	//	hash := fmt.Sprintf("%x", md5.Sum([]byte(toHash)))
+	//	if string(hash[0:5]) == "00000" {
+	//		fmt.Printf("Found %v in hash %v\n", index, hash)
+	//		code = code + string(hash[5])
+	//	}
+	//	//if index % 1000 == 0 {
+	//	//	fmt.Printf("index: %v, Hash: %v\n", index, hash[0:5])
+	//	//}
+	//	index++
+	//}
+	//fmt.Println(code)
+
+	//Part 2
 	index := 0
-	var code string
+	code := make(map[int]string)
 	for len(code) < 8 {
 		toHash := fmt.Sprintf("%v%v", input, index)
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(toHash)))
 		if string(hash[0:5]) == "00000" {
-			fmt.Printf("Found %v in hash %v\n", index, hash)
-			code = code + string(hash[5])
+			if hash[5] >= byte('a') {
+				index++
+				continue
+			}
+			pos64, _ := strconv.ParseInt(string(hash[5]), 10, 64)
+			pos := int(pos64)
+			if _, ok := code[pos]; !ok {
+				if pos < 8 {
+					code[pos] = string(hash[6])
+					fmt.Printf("Found %v in hash %v\n", index, hash)
+					fmt.Printf("Putting %v in space %v\n", string(hash[6]), pos)
+				}
+			}
 		}
-		//if index % 1000 == 0 {
-		//	fmt.Printf("index: %v, Hash: %v\n", index, hash[0:5])
-		//}
 		index++
 	}
 	fmt.Println(code)
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%v", code[i])
+	}
+
 }
