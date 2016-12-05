93a8b8a56433727b087d37496a3ead0cbf5f0618
Andrew Harrison <aharrison1@underarmour.com>
2016-12-05 00:15:54 -0600

    Day 5 Part 2 with animation

--- src/adventofcode/day05.go
+++ src/adventofcode/day05.go
@@ -29,6 +29,34 @@ func Day05() {
 	//fmt.Println(code)
 
 	//Part 2
+	//index := 0
+	//code := make(map[int]string)
+	//for len(code) < 8 {
+	//	toHash := fmt.Sprintf("%v%v", input, index)
+	//	hash := fmt.Sprintf("%x", md5.Sum([]byte(toHash)))
+	//	if string(hash[0:5]) == "00000" {
+	//		if hash[5] >= byte('a') {
+	//			index++
+	//			continue
+	//		}
+	//		pos64, _ := strconv.ParseInt(string(hash[5]), 10, 64)
+	//		pos := int(pos64)
+	//		if _, ok := code[pos]; !ok {
+	//			if pos < 8 {
+	//				code[pos] = string(hash[6])
+	//				fmt.Printf("Found %v in hash %v\n", index, hash)
+	//				fmt.Printf("Putting %v in space %v\n", string(hash[6]), pos)
+	//			}
+	//		}
+	//	}
+	//	index++
+	//}
+	//fmt.Println(code)
+	//for i := 0; i < 8; i++ {
+	//	fmt.Printf("%v", code[i])
+	//}
+
+	//Part 2, With animation
 	index := 0
 	code := make(map[int]string)
 	for len(code) < 8 {
@@ -44,16 +72,25 @@ func Day05() {
 			if _, ok := code[pos]; !ok {
 				if pos < 8 {
 					code[pos] = string(hash[6])
-					fmt.Printf("Found %v in hash %v\n", index, hash)
-					fmt.Printf("Putting %v in space %v\n", string(hash[6]), pos)
 				}
 			}
 		}
 		index++
+		if index%1000 == 0 {
+			fmt.Printf("\r")
+			for i := 0; i < 8; i++ {
+				if _, ok := code[i]; ok {
+					fmt.Printf("%v", code[i])
+				} else {
+					fmt.Printf("%s", string(hash[i+9]))
+				}
+			}
+		}
 	}
-	fmt.Println(code)
+	fmt.Printf("\r")
 	for i := 0; i < 8; i++ {
 		fmt.Printf("%v", code[i])
 	}
+	fmt.Println()
 
 }
